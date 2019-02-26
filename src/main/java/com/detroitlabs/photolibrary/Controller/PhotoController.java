7a5d1c9372b1ce3cd0a62aa907623ca2bbe549f2
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-02-26 16:04:51 -0500

    Adds basic structure for Spring Photo Library project

---
+++ src/main/java/com/detroitlabs/photolibrary/Controller/PhotoController.java
@@ -0,0 +1,7 @@
+package com.detroitlabs.photolibrary.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PhotoController {
+}
---
+++ src/main/java/com/detroitlabs/photolibrary/Model/Photo.java
@@ -0,0 +1,12 @@
+package com.detroitlabs.photolibrary.Model;
+
+public class Photo {
+
+
+
+
+
+
+
+
+}
