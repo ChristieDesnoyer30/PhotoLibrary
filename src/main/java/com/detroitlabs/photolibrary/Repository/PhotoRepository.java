fb2d3419c1d89375a36739d9c918b678ea88e058
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-02-27 17:39:29 -0500

    Updates home page to format images to the same size

--- src/main/java/com/detroitlabs/photolibrary/Repository/PhotoRepository.java
+++ src/main/java/com/detroitlabs/photolibrary/Repository/PhotoRepository.java
@@ -10,11 +10,24 @@ import java.util.List;
 @Component
 public class PhotoRepository {
 
-    private static final List<Photo> ALL_PHOTOS = Arrays.asList(new Photo("The Compiler is Scary"
+    private static final List<Photo> ALL_PHOTOS = Arrays.asList(
+            new Photo("The Compiler is Scary"
             , "afraidofthecompiler.png", LocalDate.of(2019, 2, 24)
-            , "scared"),
-            new Photo("That Look When....", "debuggeron.jpg"
-                    , LocalDate.of(2019, 02,14), "nervous"));
+            , "scared")
+            , new Photo("That Look When....", "debuggeron.jpg"
+                    , LocalDate.of(2019, 02,14), "nervous")
+            , new Photo("Programmer's fitness", "howprogrammersrun.jpeg"
+                    , LocalDate.of(2019,02,27), "funny")
+            ,new Photo("Just Install Java", "installjava.jpg",LocalDate.of(2019,02,25)
+                    ,"funny")
+            ,new Photo("No Comment...","nocomment.jpg", LocalDate.of(2019,02,27)
+                    , "confused")
+            , new Photo("No Idea", "noidea.jpg", LocalDate.of(2019,2,25)
+                    , "confused"),
+            new Photo("Sorting arrays...", "arraysorter.jpg" , LocalDate.of(2019, 2,28)
+                    ,"confused  nervous")
+
+    );
 
 
 
